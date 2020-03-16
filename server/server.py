938eb681af117001fa2f6308e9e9a71f9d3006bc
lucky15987 <19988843+lucky15987@users.noreply.github.com>
2020-03-16 03:16:18 -0400

    Updated server with comments, accept client_socket.

--- server/server.py
+++ server/server.py
@@ -9,8 +9,13 @@ port = 5432
 server_socket.bind((host, port)) #ip address of the host's current pc/device
 print('Server: Socket conneted to ' + host)
 
-server_socket.listen(0)  #generate a listener for connection
+server_socket.listen(3)  #generate a listener for 3 connections (clients)
+print('Waiting for connections')
 
 while True:
+    client_socket, addr = server_socket.accept()
+    print('Connected with ', addr)
+
     n = server_socket.recvfrom(2040)
 
+    server_socket.close()
