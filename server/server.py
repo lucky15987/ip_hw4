e140ea48b5938fa8489ff544a8c2a897553e3cad
lucky15987 <19988843+lucky15987@users.noreply.github.com>
2020-03-16 02:55:36 -0400

    Imported socket. Created host, port, and basic server framework.

--- server/server.py
+++ server/server.py
@@ -1,1 +1,14 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print('Server: Socket Created')
+
+host = 'localhost'
+port = 5432
+
+server_socket.bind((host, port)) #ip address of the host's current pc/device
+print('Server: Socket conneted to ' + host)
+
+while True:
+    n = server_socket.recvfrom(2040)
 
