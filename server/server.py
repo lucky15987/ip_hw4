37f1ad22b84aa0e3633f0ea012ed35ce443cb587
lucky15987 <19988843+lucky15987@users.noreply.github.com>
2020-03-16 03:27:54 -0400

    Updated server socket

--- server/server.py
+++ server/server.py
@@ -6,16 +6,17 @@ print('Server: Socket Created')
 host = 'localhost'
 port = 5432
 
-server_socket.bind((host, port)) #ip address of the host's current pc/device
+
+server_socket.connect(host, port) #use connect for UDP sockets
 print('Server: Socket conneted to ' + host)
 
-server_socket.listen(3)  #generate a listener for 3 connections (clients)
+#server_socket.listen(3)  #generate a listener for 3 connections (clients)
 print('Waiting for connections')
 
 while True:
-    client_socket, addr = server_socket.accept()
+    (client_socket, addr) = server_socket.accept()
     print('Connected with ', addr)
 
-    n = server_socket.recvfrom(2040)
+    #n = server_socket.recvfrom(2040)
 
     server_socket.close()
