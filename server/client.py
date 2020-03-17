6c641f16ff52d7c663aa19ae96df483ba33a018f
lucky15987 <19988843+lucky15987@users.noreply.github.com>
2020-03-16 20:40:10 -0400

    Added additional "client_socket.recvfrom()" variables to handle incoming data from server. Included verification messages that notifies the user that the data has been sent to the server.

--- server/client.py
+++ server/client.py
@@ -1,22 +1,29 @@
 import socket
 
+
+
+
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print('Client: Socket Created')
 
 host = 'localhost'
 port = 5432
 
-#client_socket.connect((host, port))
-
-msg = "Test Succesful"
+num = input('Enter a number between 0-10: ')
+n = str.encode(num)
 
 try:
-    client_socket.sendto(msg.encode(), ('localhost', 5432))
+    client_socket.sendto(n, ('localhost', 5432))
 
     data2, addr = client_socket.recvfrom(1024)
-    print('Message Recieved: ', data2.decode())
+    print(data2.decode())
+
+    data3, addr = client_socket.recvfrom(1024)
+    print(data3.decode())
+
     client_socket.sendto("Thanks bro!".encode(), addr)
-    print('Message Sent to Server!')
+    print('Message Sent to Server')
 
 finally:
     print("Closing Client")
